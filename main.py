c11898ef509d304ba55a829a30c1b3a488ea9683
binaykumar19 <binaykumar19@gmail.com>
2019-04-01 10:27:50 -0400

    Task 2: Added code for Testing the model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import re
 import numpy as np
 
@@ -12,12 +13,11 @@ def load_words(documents, stop_words, vocabulary):
 
         data = data.lower()
         words = re.split('[^a-zA-Z]', data)
-        if 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazgvzywaaaaaaaaausuvdidyxoty' in words:
-            print(document)
+
         for word in words:
             if word not in stop_words:
                 total_words_count = total_words_count + 1
-                vocabulary.add(word)
+                vocabulary[word] = 0
                 words_frequency[word] = words_frequency.get(word,0) + 1
 
     return words_frequency, total_words_count
@@ -32,37 +32,81 @@ ham_prior = len(training_ham_documents)/(len(training_ham_documents) + len(train
 spam_prior = len(training_spam_documents)/(len(training_ham_documents) + len(training_spam_documents))
 
 
-vocabulary = set()
+vocabulary = {}
 delta = 0.5
 
 spam_words_frequency, spam_words_count = load_words(training_spam_documents, stop_words, vocabulary)
 ham_words_frequency, ham_words_count = load_words(training_ham_documents, stop_words, vocabulary)
 
-#sorting the vocabulary
-vocabulary = sorted(vocabulary)
 vocabulary_size = len(vocabulary)
-
 conditional_prob = np.zeros((2, vocabulary_size))
+model = []
+
+#sorting the vocabulary
+vocabulary_sorted = sorted(vocabulary.keys())
 
 #smoothing
 spam_words_count = spam_words_count + delta * vocabulary_size
 ham_words_count = ham_words_count + delta * vocabulary_size
-model = []
 
 #Conditional Probability
-for j in range(0, vocabulary_size):
-    word = vocabulary[j]
+word_index = 0
+for word in vocabulary_sorted:
+    vocabulary[word] = word_index
+    conditional_prob[0, word_index] = ham_words_frequency.get(word, 0) + delta / ham_words_count
+    conditional_prob[1, word_index] = spam_words_frequency.get(word, 0) + delta / spam_words_count
 
-    conditional_prob[0, j] = spam_words_frequency.get(word, 0) + delta / spam_words_count
-    conditional_prob[1, j] = ham_words_frequency.get(word, 0) + delta / ham_words_count
-
-    model_line = str(j+1)+"  "+word+"  "+ str(ham_words_frequency.get(word, 0))+"  " +\
-                    str(conditional_prob[1, j])+"  " + str(spam_words_frequency.get(word, 0)) + "  " + \
-                    str(conditional_prob[0, j])
+    model_line = str(word_index+1)+"  "+word+"  "+ str(ham_words_frequency.get(word, 0))+"  " +\
+                    str(conditional_prob[0, word_index])+"  " + str(spam_words_frequency.get(word, 0)) + "  " + \
+                    str(conditional_prob[1, word_index])
 
     model.append(model_line)
+    word_index = word_index + 1
 
+# writing model.txt for Task 1
 with open('data/model.txt', 'w') as file:
     for line in model:
         file.write(line + '\n')
 
+#Testing
+testing_documents = glob.glob("data/test/*.txt")
+testing_result = []
+document_count = 1
+
+
+for document_path in testing_documents:
+    print(document_path)
+    with open(document_path,  'r') as file:
+        data = file.read()
+    document = document_path[10:]
+    data = data.lower()
+    words = re.split('[^a-zA-Z]', data)
+
+    score_ham = math.log10(ham_prior)
+    score_spam = math.log10(spam_prior)
+    for word in words:
+        if word in vocabulary:
+            word_index = vocabulary[word]
+            score_ham = score_ham + math.log10(conditional_prob[0, word_index])
+            score_spam = score_spam + math.log10(conditional_prob[1, word_index])
+
+    if score_ham > score_spam:
+        predicted_class = "ham"
+    else:
+        predicted_class = "spam"
+
+    if (predicted_class == document[5:8] or
+            predicted_class == document[5:9]):
+        classification = "right"
+    else:
+        classification = "wrong"
+
+    result_line = str(document_count) + "  " + document + "  " + predicted_class + "   " + str(score_ham)\
+                  +"  "+ str(score_spam) +"  "+ classification
+    print(result_line)
+    testing_result.append(result_line)
+
+    document_count = document_count + 1
+
+with open('data/baseline-result.txt ', 'w') as file:
+    file.write(result_line + '\n')
